17c8c00a41d02383b4ea939cd2308d7af6cb0098
Yasir Arafath <rony1382006@gmail.com>
2021-07-13 17:06:36 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import { connect } from "react-redux";
+import Counter from "./Counter";
+
+//Map Redux state to component props
+function mapStateToProps(state){
+    return{
+        countValue: state.count
+    }
+}
+
+//Action
+var increaseAction = {type: 'increase'};
+var decreaseAction = {type: 'decrease'};
+
+//Map Redux actions to component props
+
+function mapDispatchToProps(dispatch){
+    return{
+        increaseCount: function(){
+            return dispatch(increaseAction);
+        },
+        decreaseCount: function(){
+            return dispatch(decreaseAction);
+        }
+    }
+}
+
+//HOC
+var connectedComponent = connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Counter);
+
+export default connectedComponent;
